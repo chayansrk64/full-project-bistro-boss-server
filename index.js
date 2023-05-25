b444d009710e64914fd069d19e8b79aa784d6e29
chayansrk64 <chayansrk64@gmail.com>
2023-05-25 10:55:21 +0600

    reviews done

--- index.js
+++ index.js
@@ -30,9 +30,17 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db('bistroDB').collection('menu');
+    const reviewCollection = client.db('bistroDB').collection('reviews');
 
+     // menu
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+
+    // reviews
+    app.get('/reviews', async(req, res) => {
+        const result = await reviewCollection.find().toArray();
         res.send(result)
     })
 
