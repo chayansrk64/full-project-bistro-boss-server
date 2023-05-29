dd0e8a69248dc932033a84fe9e4d25cca65fd5b1
chayansrk64 <chayansrk64@gmail.com>
2023-05-29 11:04:01 +0600

    delete operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -60,9 +60,15 @@ async function run() {
 
     app.post('/carts', async(req, res) => {
         const item = req.body;
-        console.log(item);
         const result = await cartCollection.insertOne(item);
         res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await cartCollection.deleteOne(query);
+        res.send(result)
     })
 
     // Send a ping to confirm a successful connection
