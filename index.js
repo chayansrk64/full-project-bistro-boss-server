e40a8e061f2d4eadf68f57ce8d14b1acced0af76
chayansrk64 <chayansrk64@gmail.com>
2023-06-04 17:43:27 +0600

    reachart added

--- index.js
+++ index.js
@@ -240,6 +240,63 @@ async function run() {
     })
 
 
+  // best solution is( mongodb aggrete pipeline )
+  // order statistics (second best solution)
+
+  /**
+   * 1. load all payments
+   * 2. for each payment, get the menuItems array
+   * 3. for each item in the menuItems array ge the menuItem form the menu collection
+   * 4. put then in an array: allOrderedItems
+   * 5. separate ollOrderdItems by category using filter
+   * 6. now get the quantity by using length: pizzas.length
+   * 7 for each category use reduce to get the total amount spent on this category
+   * 
+  */
+
+
+  app.get('/order-stats', verifyJWT, verifyAdmin, async(req, res) =>{
+    const pipeline = [
+      {
+        $lookup: {
+          from: 'menu',
+          localField: 'menuItems',
+          foreignField: '_id',
+          as: 'menuItemsData'
+        }
+      },
+      {
+        $unwind: '$menuItemsData'
+      },
+      {
+        $group: {
+          _id: '$menuItemsData.category',
+          count: { $sum: 1 },
+          total: { $sum: '$menuItemsData.price' }
+        }
+      },
+      {
+        $project: {
+          category: '$_id',
+          count: 1,
+          total: { $round: ['$total', 2] },
+          _id: 0
+        }
+      }
+      
+    ];
+
+    const result = await paymentCollection.aggregate(pipeline).toArray()
+    res.send(result)
+
+  })
+
+
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
